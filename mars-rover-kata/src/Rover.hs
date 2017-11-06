0c35eafae5b467e34874f7f347d34d8229996939
Liam Griffin <gryffskin@gmail.com>
2017-11-06 22:07:31 +0000

    Use Either for Position, use foldM over commands

--- mars-rover-kata/src/Rover.hs
+++ mars-rover-kata/src/Rover.hs
@@ -1,6 +1,6 @@
-module Rover
-    ( move
-    ) where
+module Rover where
+
+import Control.Monad
 
 data Direction = North | East | South | West
 type Position = (Int, Int, Direction)
@@ -9,20 +9,20 @@ type Obstacle = (Int, Int)
 move :: String -> [Obstacle] -> String
 move commands obstacles = toString (movement commands obstacles initialPosition)
 
-movement :: String -> [Obstacle] -> Position -> Position
-movement commands obstacles position = foldl (executeCommand obstacles) position commands
+movement :: String -> [Obstacle] -> Position -> Either Position Position
+movement commands obstacles position = foldM (executeCommand obstacles) position commands
 
-executeCommand :: [Obstacle] -> Position -> Char -> Position
+executeCommand :: [Obstacle] -> Position -> Char -> Either Position Position
 executeCommand _ position command
   | command == 'M' = moveForward position
-  | command == 'L' = rotateLeft position
-  | command == 'R' = rotateRight position
+  | command == 'L' = Right $ rotateLeft position
+  | command == 'R' = Right $ rotateRight position
 
-moveForward :: Position -> Position
-moveForward (x, y, North) = (x, (y + 1) `mod` 10, North)
-moveForward (x, y, East) = ((x + 1) `mod` 10, y, East)
-moveForward (x, y, West) = ((x - 1) `mod` 10, y, West)
-moveForward (x, y, South) = (x, (y - 1) `mod` 10, South)
+moveForward :: Position -> Either Position Position
+moveForward (x, y, North) = Right (x, (y + 1) `mod` 10, North)
+moveForward (x, y, East)  = Right ((x + 1) `mod` 10, y, East)
+moveForward (x, y, West)  = Right ((x - 1) `mod` 10, y, West)
+moveForward (x, y, South) = Right (x, (y - 1) `mod` 10, South)
 
 rotateLeft :: Position -> Position
 rotateLeft (x, y, North) = (x, y, West)
@@ -39,8 +39,8 @@ rotateRight (x, y, West) = (x, y, North)
 initialPosition :: Position
 initialPosition = (0, 0, North)
 
-toString :: Position -> String
-toString (x, y, dir) = show x ++ "," ++ show y ++ "," ++ dirToString dir
+toString :: Either Position Position -> String
+toString (Right (x, y, dir)) = show x ++ "," ++ show y ++ "," ++ dirToString dir
 
 dirToString :: Direction -> String
 dirToString North = "N"
