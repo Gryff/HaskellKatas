3aecba7cf8ca40623fa144dc5eca7a4d36afa1b9
Liam Griffin <gryffskin@gmail.com>
2019-01-30 11:41:11 +0000

    Make a Transaction hold an Int instead of being generic

--- bank-kata/src/Bank.hs
+++ bank-kata/src/Bank.hs
@@ -2,23 +2,23 @@ module Bank where
 
 import Control.Monad.State
 
-data Transaction a = Deposit a | Withdrawal a deriving (Eq, Show)
+data Transaction = Deposit Int | Withdrawal Int deriving (Eq, Show)
 
 newBank = []
 
-deposit :: Int -> State [Transaction Int] ()
+deposit :: Int -> State [Transaction] ()
 deposit amount = state $ \transactions -> ((), transactions ++ [Deposit amount])
 
-withdraw :: Int -> State [Transaction Int] ()
+withdraw :: Int -> State [Transaction] ()
 withdraw amount = state $ \transactions -> ((), transactions ++ [Withdrawal amount])
 
-getStatement :: State [Transaction Int] String
+getStatement :: State [Transaction] String
 getStatement = state $ \transactions -> (toStatement transactions, transactions)
 
-toStatement :: [Transaction Int] -> String
+toStatement :: [Transaction] -> String
 toStatement transactions = unlines $ map stringifyTransaction transactions
 
-stringifyTransaction :: Transaction Int -> String
+stringifyTransaction :: Transaction -> String
 stringifyTransaction (Deposit amount) = "Desposited " ++ (show amount)
 stringifyTransaction (Withdrawal amount) = "Withdrew " ++ (show amount)
 
