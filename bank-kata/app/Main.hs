c1ae09046c4844b70340a292bbd5167f36126303
Liam Griffin <gryffskin@gmail.com>
2019-01-30 16:57:55 +0000

    Put example usage in Main.hs

--- bank-kata/app/Main.hs
+++ bank-kata/app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
+import Control.Monad.State
+
 import Bank
 
+newBank = []
+
 main :: IO ()
-main = return ()
+main = do
+  let statement = evalState doTheStuff newBank
+  putStr statement
+
+doTheStuff = do
+  deposit 200
+  withdraw 100
+  deposit 3000
+  getStatement
